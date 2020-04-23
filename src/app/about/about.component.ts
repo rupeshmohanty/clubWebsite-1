f21703231e686ba65d820231c06b6862fdcd9a95
Tri <trideep.india@gmail.com>
2020-04-23 13:40:45 +0530

    Routing Correction

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,8 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  loadScripts() {
+    const dynamicScripts = [
+      "assets/js/jquery.min.js",
+      "assets/js/jquery.easing.1.3.js",
+      "assets/js/bootstrap.min.js",
+      "assets/js/jquery.waypoints.min.js",
+      "assets/js/jquery.flexslider-min.js",
+      "assets/js/sticky-kit.min.js",
+      "assets/js/owl.carousel.min.js",
+      "assets/js/jquery.countTo.js",
+      "assets/js/main.js",
+      "assets/js/modernizr-2.6.2.min.js"
+    ];
+    for (let i = 0; i < dynamicScripts.length; i++) {
+      const node = document.createElement('script');
+      node.src = dynamicScripts[i];
+      node.type = 'text/javascript';
+      node.async = false;
+      node.charset = 'utf-8';
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
+  }
 
-  constructor() { }
+  constructor() {
+    this.loadScripts();
+   }
 
   ngOnInit(): void {
   }
